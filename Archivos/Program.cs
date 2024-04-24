7c093bcaf7827634865223101ef52199f186f872
Diegolew <diegolewensztain2@gmail.com>
2024-04-24 09:29:18 -0400

    Commit 5

--- Archivos/Program.cs
+++ Archivos/Program.cs
@@ -1,41 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//alggggg
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bienvenido a la Calculadora de Movimiento Rectilíneo");
-        Console.WriteLine("¿Qué tipo de movimiento desea calcular?");
-        Console.WriteLine("1. MRU");
-        Console.WriteLine("2. MRUA");
-        int tipoMovimiento = Convert.ToInt32(Console.ReadLine());
+        bool continuar = true;
 
-        Console.WriteLine("¿Desea calcular distancia, velocidad o tiempo?");
-        Console.WriteLine("1. Distancia");
-        Console.WriteLine("2. Velocidad");
-        Console.WriteLine("3. Tiempo");
-        int tipoCalculo = Convert.ToInt32(Console.ReadLine());
-
-        double resultado = 0;
-
-        if ((tipoMovimiento == 1) && (tipoCalculo == 2))
+        while (continuar)
         {
-            Console.WriteLine("Ingrese la velocidad (m/s):");
-            double velocidad = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bienvenido a la Calculadora de Movimiento Rectilíneo");
+            Console.WriteLine("¿Qué tipo de movimiento desea calcular?");
+            Console.WriteLine("1. MRU");
+            Console.WriteLine("2. MRUA");
+ 
 
-            Console.WriteLine("Ingrese el tiempo (s):");
-            double tiempo = Convert.ToDouble(Console.ReadLine());
-
-            resultado = velocidad * tiempo;
-            Console.WriteLine($"Resultado: La velocidad es de {resultado} m/s.");
         }
-        else
-        {
-            Console.WriteLine("Opción no válida.");
-        }
-
-        Console.WriteLine("Gracias por usar la Calculadora de Movimiento Rectilíneo.");
     }
 }
